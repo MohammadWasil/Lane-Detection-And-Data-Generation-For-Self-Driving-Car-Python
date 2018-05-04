a69628c38a6334b5ac147a8de0de46385ecaa844
MohammadWasil <31696557+MohammadWasil@users.noreply.github.com>
2018-05-05 03:38:01 +0530

    Update NetworkDataGen.cs

--- SDC Lane Detection And Data Generation/Assets/Scripts/NetworkDataGen.cs
+++ SDC Lane Detection And Data Generation/Assets/Scripts/NetworkDataGen.cs
@@ -37,7 +37,6 @@ public class NetworkDataGen : MonoBehaviour {
 
 	string message = "Wasil"; 		// The message to send back.
 
-	//Vector3 pos = Vector3.zero;
 	bool running;
 
 	private void Start()
@@ -107,46 +106,7 @@ public static string GetLocalIPAddress()
 		//nwStream.Write(buffer, 0, bytesRead);
 		Debug.Log (dataReceived);
 
-
-		/*
-	if (dataReceived != null)
-	{
-		if (dataReceived == "stop") {
-			running = false;
-		}
-	}
-	else
-	{
-	*/
-		//nwStream.Write(buffer, 0, bytesRead);
-		//pos =  StringToVector3(dataReceived);
-		//print("moved");
-
-
-		//}
 	}
 
-
-	/*
-	public static Vector3 StringToVector3(string sVector)
-	{
-	// Remove the parentheses
-	if (sVector.StartsWith("(") && sVector.EndsWith(")"))
-	{
-		sVector = sVector.Substring(1, sVector.Length - 2);
-	}
-
-	// split the items
-	string[] sArray = sVector.Split(',');
-
-	// store as a Vector3
-	Vector3 result = new Vector3(
-		float.Parse(sArray[0]),
-		float.Parse(sArray[1]),
-		float.Parse(sArray[2]));
-
-	return result;
-	}
-	*/
 }
 
